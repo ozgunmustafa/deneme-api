e1b09532643db4a0703ab3f283fba4bf9bc04d1a
ozgunmustafa <34939878+ozgunmustafa@users.noreply.github.com>
2020-08-11 12:56:50 +0300

    Update connectDB.js

--- helpers/database/connectDB.js
+++ helpers/database/connectDB.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const connectDB = () => {
 
   mongoose
-    .connect(process.env.MONGO_URI, {
+    .connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useFindAndModify: false,
       useCreateIndex: true,
